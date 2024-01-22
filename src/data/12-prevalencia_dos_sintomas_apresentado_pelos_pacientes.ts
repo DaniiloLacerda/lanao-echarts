1bbd799447eb97eb2ec59d24a042117514dfb8d4
Danilo Lacerda <danilolacerda@Danilos-MacBook-Pro.local>
2024-01-22 16:47:04 -0300

    ajuste de titulo

--- src/data/12-prevalencia_dos_sintomas_apresentado_pelos_pacientes.ts
+++ src/data/12-prevalencia_dos_sintomas_apresentado_pelos_pacientes.ts
@@ -1,30 +1,43 @@
 export const option12 = {
   title: {
-   text: "Prevalência das comorbidades apresentada pelos paciente no momento de realização do exame.",
-   left: "left",
- },
- 
- xAxis: {
-   type: 'category',
-   data: ['Tosse', 'Falta de ar', 'Hiperemia Ocular', 'Edema ocular', 'Febre', 'Sibilância', 'Prurido nasal', 'Coriza nasal', 'Dor', 'Espirro', 'Exacerbação D.A.', 'Nega'],
-   axisLabel: { interval: 0, rotate: 30 }
- },
- yAxis: {
-   type: 'value'
- },
- label: {
-       show: true,
-       fontSize: 14,
-       fontWeight: "bold",
- },
- series: [
-   {
-     data: [21, 8, 1, 1, 3, 3, 1, 3, 3, 1, 1, 109],
-     type: 'bar',
-     showBackground: true,
-     backgroundStyle: {
-       color: 'rgba(180, 180, 180, 0.2)'
-     }
-   }
- ]
+    text: "Prevalência dos sintomas apresentados pelos paciente.",
+    left: "left",
+  },
+
+  xAxis: {
+    type: "category",
+    data: [
+      "Tosse",
+      "Falta de ar",
+      "Hiperemia Ocular",
+      "Edema ocular",
+      "Febre",
+      "Sibilância",
+      "Prurido nasal",
+      "Coriza nasal",
+      "Dor",
+      "Espirro",
+      "Exacerbação D.A.",
+      "Nega",
+    ],
+    axisLabel: { interval: 0, rotate: 30 },
+  },
+  yAxis: {
+    type: "value",
+  },
+  label: {
+    show: true,
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  series: [
+    {
+      data: [21, 8, 1, 1, 3, 3, 1, 3, 3, 1, 1, 109],
+      type: "bar",
+      showBackground: true,
+      backgroundStyle: {
+        color: "rgba(180, 180, 180, 0.2)",
+      },
+    },
+  ],
 };
